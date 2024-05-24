3e18068d1be8bd2e8b12f128bc0bffc7569624f9
Mirai Zhao <miraizhao@qq.com>
2024-05-24 17:57:01 +0800

    0524

--- src/components/Icon/Icon.vue
+++ src/components/Icon/Icon.vue
@@ -1,15 +1,45 @@
 <template>
-    <div>
-      icon组件ss
-    </div>
+  <i class="my-icon">
+    <font-awesome-icon v-bind="filiterProps" />
+  </i>
 </template>
 
 <script setup lang="ts">
+import { omit } from 'radash';
+import { computed } from 'vue';
 defineOptions({
-    name: "MyIcon"
+  name: "MyIcon"
 })
+interface Props {
+  icon: string,
+  size?: string,
+  rotation?: number | string,
+  flip?: string,
+  beat?: boolean,
+  "beat-fade"?: boolean,
+  bounce?: boolean,
+  fade?: boolean,
+  spin?: boolean,
+  "spin-reverse"?: boolean,
+  "spin-pulse"?: boolean,
+  type?: string,
+  color?: string,
+}
+const prpos = defineProps<Props>()
 
+const filiterProps = computed(() => omit(prpos, ['type']))
 
 </script>
 
-<style scoped></style>
+<style scoped lang="scss">
+.my-icon {
+  --my-icon-color: inherit;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  fill: currentColor;
+  color: var(--my-icon-color);
+  font-size: inherit;
+}
+</style>
--- src/views/IconView.vue
+++ src/views/IconView.vue
@@ -1,6 +1,15 @@
 <template>
-    <div>
-        <MyIcon/>
+    <h1 class="row-title">普通引入</h1>
+    <div class="row">
+        <MyIcon icon="user-secret" />
+        <MyIcon icon="bars" />
+        <MyIcon icon="shield-halved" />
+        <MyIcon icon="file" />
+        <MyIcon icon="filter" />
+        <MyIcon icon="gear" />
+        <MyIcon icon="barcode" />
+        <MyIcon icon="folder" />
+        <MyIcon icon="folder-open" />
     </div>
 </template>
 
@@ -9,4 +18,18 @@ import MyIcon from '@/components/Icon/Icon.vue'
 
 </script>
 
-<style scoped></style>
+<style scoped lang="scss">
+.row-title {
+    font-size: 20px;
+    margin-bottom: 10px;
+
+}
+
+.row {
+    display: flex;
+
+    .my-icon {
+        flex: 1
+    }
+}
+</style>
